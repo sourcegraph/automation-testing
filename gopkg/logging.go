dbe56029f8a129d1c83d58b8bfbc39df1c216086
Sourcegraph <campaigns@sourcegraph.com>
2020-09-14 13:40:45 +0000

    Fix import path for log15 package

--- gopkg/logging.go
+++ gopkg/logging.go
@@ -1,6 +1,6 @@
 package main
 
-import "gopkg.in/inconshreveable/log15.v2"
+import "github.com/inconshreveable/log15"
 
 func LogWarn(msg string) {
 	log15.Warn(msg, "repository", "automation-testing")
